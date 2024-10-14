4d6572d394fc2cf73a56d3d13695db74b8bc0670
sankaaaa <olexa2404@gmail.com>
2024-10-15 02:51:42 +0300

    done if, move_semantics, quiz1

--- rustlings/exercises/if/if2.rs
+++ rustlings/exercises/if/if2.rs
@@ -1,16 +1,14 @@
 // if2.rs
 
-// Step 1: Make me compile!
-// Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
-// Execute the command `rustlings hint if2` if you want a hint :)
-
-// I AM NOT DONE
+// done
 
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
