ae9c232c59d1086a9a5480cff004bd03caaf2b05
sankaaaa <olexa2404@gmail.com>
2024-10-16 02:08:41 +0300

    done strings, quiz2, errors

--- rustlings/exercises/error_handling/errors4.rs
+++ rustlings/exercises/error_handling/errors4.rs
@@ -1,7 +1,5 @@
 // errors4.rs
-// Make this test pass! Execute `rustlings hint errors4` for hints :)
-
-// I AM NOT DONE
+// done
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -14,7 +12,15 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        if value<0 {
+        Err(CreationError::Negative)
+        }
+        else if value == 0 {
+        Err(CreationError::Zero)
+        }
+        else {
         Ok(PositiveNonzeroInteger(value as u64))
+        }
     }
 }
 
