6f5b02b77cedcdd2cca046cf3a719379b085a4ad
sankaaaa <olexa2404@gmail.com>
2024-10-16 03:43:00 +0300

    done arc, box, generics, option, quiz3, tests, traits

--- rustlings/exercises/traits/traits1.rs
+++ rustlings/exercises/traits/traits1.rs
@@ -1,21 +1,16 @@
 // traits1.rs
-// Time to implement some traits!
-//
-// Your task is to implement the trait
-// `AppendBar' for the type `String'.
-//
-// The trait AppendBar has only one function,
-// which appends "Bar" to any object
-// implementing this trait.
-
-// I AM NOT DONE
+// done
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
+    fn append_bar(self) -> Self {
+        let mut changeable = self;
+        changeable.push_str("Bar");
+        changeable
+    }
 }
 
 fn main() {
